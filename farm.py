a67b9e916baf9944f9882815fc874491664239ec
Samuel Yoon <samuelyoon17@gmail.com>
2023-12-16 13:34:18 -0500

    Updated environmental vars

--- farm.py
+++ farm.py
@@ -8,24 +8,25 @@ class Farm:
         self.time = gametime
         self.farmlocation = farmlocation
         self.plots = [
-            Crop_Plot("carrots", .99, 1, 20, 20),
+            Crop_Plot("carrots", 0.99, 1, 20, 20),
             Crop_Plot("lettuce", 1.49, 1, 23, 23),
-            Crop_Plot("wheat", .45, 1, 12, 12),
+            Crop_Plot("wheat", 0.45, 1, 12, 12),
             Crop_Plot("apples", 1.99, 1, 25, 25),
         ]
-    
-    def update_Farm():
+
+    # def get_user_input
+
+    def update_Farm(self):
         weather = Enviroment.rand_weather()
         pests = Enviroment.rand_pests()
-        temprature = Enviroment.rand_temp()     
-        
-    
+        temprature = Enviroment.rand_temp()
 
-    # def create_plot(self, name, unit_price, crop_num, growth_time):
-    #     new_plot = Crop_Plot(name, unit_price, crop_num, growth_time)
-    #     self.plots.append(new_plot)
+        # if weather == 'sunlight':
+        #     for i in self.plots:
+        #         i.days_until_harvest -= 
+        # elif weather == 'rain':
 
-    # def __repr__(self) -> str:
-    #     farm_details = ""
-    #     for i in self.plots:
-    #         farm_details += f"{i.name} "
+
+
+        # Advance daycount +1
+        gametime += 1
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from farm import Farm
 
-plots = []
-
 bobs_farm = Farm("Bob", 0, "Maryland")
+
+while bobs_farm.time < 40:
+    bobs_farm.get_user_input()
+    bobs_farm.update_Farm()
