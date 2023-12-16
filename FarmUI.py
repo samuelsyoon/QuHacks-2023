23998cc54cb2cb0a70b4e905c1ff0edc0a045e4b
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2023-12-16 13:56:17 -0500

    added more stuff to ui

--- FarmUI.py
+++ FarmUI.py
@@ -1,13 +1,31 @@
+#import modules
 import tkinter as tk
 import customtkinter as ctk
 
-ctk.set_appearance_mode("System")
+#define functions
+def switch_screen():
+    print("temp")
+
+#set up base
+ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 
-base = ctk.CTk()
-base.geometry("760x900")
+start_window = ctk.CTk()
+start_window.geometry("1920x1080")
 
-frame = ctk.CTkFrame(master=base)
+frame = ctk.CTkFrame(master=start_window)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-base.mainloop()
+start_window.title("Farming Simulator")
+
+#Widget Configuration
+play_button = ctk.CTkButton(master=start_window, text="Play",)
+play_button.place(relx=.5, rely=.5, anchor=ctk.CENTER)
+
+start_title = ctk.CTkLabel(master=start_window, text="Farming Simulator", fg_color="transparent")
+start_title.place(relx=.5, rely=.1, anchor=ctk.CENTER)
+
+#Set frames
+
+#Run Program
+start_window.mainloop()
